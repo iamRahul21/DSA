e9bdb40cd163a0caa7bed539f4fe9a0d4c9817f1
iamRahul21 <rahuldeka072@gmail.com>
2024-06-15 13:56:47 +0530

    insertion sort

---
+++ sorting/insertionSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertion_sort(vector<int> &arr, int n) {
+    for (int i=0; i<=n-1; i++) {
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]) {
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }  
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i=0; i<n; i++)
+        cin >> arr[i];
+
+    insertion_sort(arr, n);
+
+    for (int i=0; i<n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
